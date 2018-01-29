8ba2af9390d63310119457120f5b83df5b5c487b
smit sheth <smit.sheth@mobiliya.com>
2018-01-29 12:03:22 +0530

    Change @microservice tag in license header
    Change the @microservice tag from device-bacnet to device-bluetooth in
    license header
    
    Signed-off-by: smit sheth <smit.sheth@mobiliya.com>

--- src/main/java/org/edgexfoundry/service/ProvisionService.java
+++ src/main/java/org/edgexfoundry/service/ProvisionService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @microservice:  device-bluetooth
+ * @microservice:  device-bacnet
  * @version: 1.0.0
  *******************************************************************************/
 package org.edgexfoundry.service;
