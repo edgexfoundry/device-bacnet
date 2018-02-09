006a6d8620299ceb95fc231f19591a8b32b760de
smit sheth <smit.sheth@mobiliya.com>
2018-02-09 17:21:10 +0530

    Add discovery mechanism implementation
    This change adds implementation of discovery mechanism
    
    Signed-off-by: smit sheth <smit.sheth@mobiliya.com>

--- python/app/BACNetDriver.py
+++ python/app/BACNetDriver.py
@@ -33,7 +33,7 @@ from bacpypes.apdu import (ReadPropertyMultipleRequest,
                            WhoIsRequest,
                            WritePropertyRequest,
                            IAmRequest)
-from bacpypes.primitivedata import Null, Atomic, Integer, Unsigned, Real
+from bacpypes.primitivedata import Null, Atomic, Integer, Unsigned, Real, CharacterString
 from bacpypes.constructeddata import Array, Any
 from bacpypes.basetypes import PropertyIdentifier, ServicesSupported
 from bacpypes.errors import DecodingError
@@ -51,8 +51,11 @@ def StartServer():
     _ss.run()
     return _ss
 
-class BACNetDriver():
-    def __init__(self):
+# some debugging
+_debug = 0
+
+class BACNetDriver(BIPSimpleApplication):
+    def __init__(self, *args):
         # register signal handler
         atexit.register(self._interrupt)
         # threading init
@@ -67,7 +70,8 @@ class BACNetDriver():
         self.bacnet_app = None
         self._bacnet_thread = threading.Thread(target=self._run_bacnet)
         self._bacnet_thread.daemon = True
-        
+        self._request = None
+
     def run(self):
         self._load_config()
         self._init_bacnet()
@@ -118,8 +122,8 @@ class BACNetDriver():
         print("host %s" % hostname)
         addr = socket.gethostbyname(hostname)
         print("bacnet stack using %s" % (addr + suffix))
-        self.bacnet_app = BACnet(ldo, addr + suffix)
-        
+        self.this_application = DiscoveryApplication(ldo, addr + suffix)
+
     def _value_format(self, p, v):
         return str(v)
 
@@ -153,8 +157,8 @@ class BACNetDriver():
         iocb.ioRequest = request
 
         # give it to the application to send
-        _ss.bacnet_app.do_request(request, iocb)
-
+        _ss.this_application.do_request(request, iocb)
+        
         # wait for the response
         iocb.ioComplete.wait()
 
@@ -227,8 +231,8 @@ class BACNetDriver():
         iocb.ioRequest = request
 
         # give it to the application to send
-        _ss.bacnet_app.do_request(request, iocb)
-
+        _ss.this_application.do_request(request, iocb)
+        
         # wait for the response
         iocb.ioComplete.wait()
 
@@ -240,19 +244,34 @@ class BACNetDriver():
 
         return n
 
-        # each block gets a completion event
-        self.ioComplete = threading.Event()
-        self.ioComplete.clear()
-        
-# BACnet application
-class BACnet(BIPSimpleApplication):
+    def scan(self):
+        addr = GlobalBroadcast()
+        self.this_application.who_is(None, None, addr)
+        return self.this_application.getdevice_discovered()
+#
+#   DiscoveryApplication
+#
+class DiscoveryApplication(BIPSimpleApplication):
 
     def __init__(self, *args):
         BIPSimpleApplication.__init__(self, *args)
+        # keep track of requests to line up responses
+        self.who_is_request = None
         # assigning invoke identifiers
         self.nextInvokeID = 1
         # keep track of requests to line up responses
         self.iocb = {}
+        
+        self.deviceinfo=None
+
+    def request(self, apdu):
+        """Sniff for Who-Is requests going downstream."""
+        # save a copy of just the Who-Is request
+        if isinstance(apdu, WhoIsRequest):
+            self.who_is_request = apdu
+
+        # forward it along
+        BIPSimpleApplication.request(self, apdu)
 
     def get_next_invoke_id(self, addr):
         initialID = self.nextInvokeID
@@ -283,7 +302,9 @@ class BACnet(BIPSimpleApplication):
         # find the request
         iocb = self.iocb.get(invoke_key, None)
         if not iocb:
-            raise RuntimeError("no matching request")
+            super()._app_complete(apdu.pduSource, apdu)
+            return;
+
         del self.iocb[invoke_key]
 
         if isinstance(apdu, Error):
@@ -329,6 +350,79 @@ class BACnet(BIPSimpleApplication):
 
         # trigger the completion event
         iocb.ioComplete.set()
+        
+    def do_IAmRequest(self, apdu):    
+        """Do something with incoming I-Am requests."""
+        if _debug: DiscoveryApplication._debug("do_IAmRequest %r", apdu)
+
+        # check for required parameters
+        if apdu.iAmDeviceIdentifier is None:
+            raise MissingRequiredParameter("iAmDeviceIdentifier required")
+        if apdu.maxAPDULengthAccepted is None:
+            raise MissingRequiredParameter("maxAPDULengthAccepted required")
+        if apdu.segmentationSupported is None:
+            raise MissingRequiredParameter("segmentationSupported required")
+        if apdu.vendorID is None:
+            raise MissingRequiredParameter("vendorID required")
+
+        # extract the device instance number
+        device_instance = apdu.iAmDeviceIdentifier[1]
+        if _debug: DiscoveryApplication._debug("    - device_instance: %r", device_instance)
+
+        # extract the source address
+        device_address = apdu.pduSource
+        if _debug: DiscoveryApplication._debug("    - device_address: %r", device_address)
+
+        # we didn't request anything yet
+        if not self.who_is_request:
+            return
+
+        if (self.who_is_request.deviceInstanceRangeLowLimit is not None) and \
+                (device_instance < self.who_is_request.deviceInstanceRangeLowLimit):
+            pass
+        elif (self.who_is_request.deviceInstanceRangeHighLimit is not None) and \
+                (device_instance > self.who_is_request.deviceInstanceRangeHighLimit):
+            pass
+        else:
+            # build a request for the object name
+            request = ReadPropertyRequest(
+                destination=apdu.pduSource,
+                objectIdentifier=apdu.iAmDeviceIdentifier,
+                propertyIdentifier='objectName',
+                )
+
+            # make an IOCB
+            iocb = IOCB(request)
+            if _debug: DiscoveryApplication._debug("    - iocb: %r", iocb)
+            # let us know when its complete
+            iocb.add_callback(self.device_discovered)
+            # give it to the application
+            self.request_io(iocb)
+
+    def getdevice_discovered(self):          
+        return self.deviceinfo
+
+    def device_discovered(self, iocb):
+        if _debug: DiscoveryApplication._debug("device_discovered %r", iocb)
+       
+        # do something for error/reject/abort
+        if iocb.ioError:
+            sys.stdout.write(str(iocb.ioError) + '\n')
+
+        # do something for success
+        elif iocb.ioResponse:
+                apdu = iocb.ioResponse
+                # should be an ack
+                if not isinstance(apdu, ReadPropertyACK):
+                        if _debug: DiscoveryApplication._debug("    - not an ack")
+                        return
+                # pull out the name
+                device_name = apdu.propertyValue.cast_out(CharacterString)
+                if _debug: DiscoveryApplication._debug("    - device_name: %r", device_name)
+                self.deviceinfo=[{"name":device_name,"address":repr(apdu.pduSource)}]
+        # do something with nothing?
+        else:
+                if _debug: DiscoveryApplication._debug("     - ioError or ioResponse expected")
 
 class Config():
 
